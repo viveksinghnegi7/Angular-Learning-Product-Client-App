1a300ce2d4ed1c61bb6e7175c3399ce3a3a4779b
Vivek Singh Negi <viveksinghnegi7@gmail.com>
2023-12-29 11:01:26 +0530

    Added login component and route

--- AngularProduct/src/app/app.routes.ts
+++ AngularProduct/src/app/app.routes.ts
@@ -1,3 +1,8 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [ 
+    {'path':'login',component:LoginComponent}    
+];
